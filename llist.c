11002941ffa69c6b537da4080ac15b251be21ec7
JuozasPetryla <juopet2@ktu.lt>
2026-02-03 17:59:14 +0200

    Encapsulating to a list, no tail handling yet

--- llist.c
+++ llist.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 #include "llist.h"
 
-extern void llist_append(Node *head, void *data) 
+extern LList* llist_init()
+{
+    Node *head = create_node(NULL);
+    Node *tail = create_node(NULL);
+
+    LList *llist = malloc(sizeof(LList));
+
+    llist->head = head;
+    llist->tail = tail;
+
+    return llist;
+}
+
+extern void llist_append(LList *llist, void *data) 
 {
     Node *ptr = create_node(data);
-    
-    ptr->next = head->next;
-    head->next = ptr;
+
+    ptr->next = llist->head->next;
+    llist->head->next = ptr;
 }
