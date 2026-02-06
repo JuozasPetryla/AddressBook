667fb2faa806109dca5fa26c7522edb5367112cf
JuozasPetryla <itsgrtjuozas@gmail.com>
2026-02-06 16:57:33 +0200

    Iterator for linked list impl

--- llist.h
+++ llist.h
@@ -1,6 +1,7 @@
 #ifndef LLIST_H
 #define LLIST_H
 
+#include <stdbool.h>
 #include "node.h"
 
 typedef struct {
@@ -8,8 +9,18 @@ typedef struct {
     Node *tail;
 } LList;
 
+typedef struct {
+    LList *llist;
+    Node *curr;
+} LListIter;
+
 void llist_prepend(LList *llist, void *data);
 void llist_append(LList *llist, void *data);
 void llist_insert(LList *llist, void *data, int pos);
+
+LListIter* llist_iter_create(LList *llist);
+bool llist_iter_condition(LListIter *llist_iter);
+void* llist_iter_next(LListIter *llist_iter);
+void llist_iter_free(LListIter *llist_iter);
 
 #endif
